6cee0a312d05f564f8be2d909bdcad8995bb6a56
lenarsaitov <lenarsaitov1@yandex.ru>
2022-11-07 00:30:16 +0300

    add hardhat tests

--- contracts/EOAccount.sol
+++ contracts/EOAccount.sol
@@ -1,50 +1,46 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity^0.8.0;
+pragma solidity^0.8.17;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EOAccount is AccessControl{
-    bytes32 public constant RELATIVE_ACCOUNTS_ROLE = keccak256("RELATIVE_ACCOUNTS_ROLE");
+contract EOAccount is AccessControl, Ownable{
+    bytes32 public constant TRUSTED_ACCOUNT_ROLE = keccak256("TRUSTED_ACCOUNT_ROLE");
 
     constructor(address[] memory relativeAccounts){
         for (uint256 i = 0; i < relativeAccounts.length; ++i) {
-            _setupRole(RELATIVE_ACCOUNTS_ROLE, relativeAccounts[i]);
+            _setupRole(TRUSTED_ACCOUNT_ROLE, relativeAccounts[i]);
             countAccount++;
         }
 
-        _setupRole(DEFAULT_ADMIN_ROLE, address(this));
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    mapping(address => uint256) balanceAccounts;
     mapping(address => uint256) voteRecover;
     uint256 public countAccount;
+
+    // fill amount
+    function fillAmount() external payable{
+    }
 
     // get total amount of contract
     function totalAmount() external view returns(uint256){
         return address(this).balance;
     }
 
-    // fil amount
-    function addAmount() external payable{
-        balanceAccounts[msg.sender] = msg.value;
-    }
-
     // start voting
-    function startRecoveryAccount(address _to) external onlyRole(RELATIVE_ACCOUNTS_ROLE) {
+    function startRecoveryAccount(address _to) external onlyRole(TRUSTED_ACCOUNT_ROLE) {
         voteRecover[_to] = 0;
     }
 
     // add vote
-    function voteRecoveryAccount(address _from, address _to) external onlyRole(RELATIVE_ACCOUNTS_ROLE) {
+    function voteRecoveryAccount(address _from, address _to) external onlyRole(TRUSTED_ACCOUNT_ROLE) {
         voteRecover[_to]++;
 
-        if (voteRecover[_to] > countAccount/2){
-            _setupRole(RELATIVE_ACCOUNTS_ROLE, _to);
-            balanceAccounts[_to] = balanceAccounts[_from];
-
-            balanceAccounts[_from] = 0;
-            _revokeRole(RELATIVE_ACCOUNTS_ROLE, _from);
+        if (voteRecover[_to] == countAccount){
+            _setupRole(DEFAULT_ADMIN_ROLE, _to);
+            _revokeRole(DEFAULT_ADMIN_ROLE, _from);
         }
     }
 }
---
+++ test/EOAccount.js
@@ -0,0 +1,125 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { string } = require("hardhat/internal/core/params/argumentTypes");
+
+let accounts
+let EAAccountsContract
+let myEAAccountsContract
+let adminRole
+let trustedRole
+
+describe("EOAccount", function (){
+  beforeEach(async function(){
+    accounts = await ethers.getSigners();
+
+    EAAccountsContract = await ethers.getContractFactory("EOAccount", accounts[0])
+    myEAAccountsContract = await EAAccountsContract.deploy([accounts[1].address, accounts[2].address, accounts[3].address])
+
+    await myEAAccountsContract.deployed()
+
+    adminRole = await myEAAccountsContract.DEFAULT_ADMIN_ROLE()
+    trustedRole = await myEAAccountsContract.TRUSTED_ACCOUNT_ROLE()
+  })
+
+  it("Owner of contract", async function(){
+    expect(await myEAAccountsContract.owner()).to.equal(accounts[0].address)
+  })
+
+  it("Fill amount is correct", async function(){
+    let amount = 1000
+    await myEAAccountsContract.fillAmount({value: amount})
+
+    expect(await myEAAccountsContract.totalAmount()).to.equal(amount)
+  })
+
+  it("Correct transfer Ownership", async function(){
+    await myEAAccountsContract.transferOwnership(accounts[5].address)
+    expect(await myEAAccountsContract.owner()).to.equal(accounts[5].address)
+  })
+
+  describe("Accounts have correct roles", function(){
+    it("Accounts have right role", async function(){
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[0].address)).to.equal(true)
+      expect(await myEAAccountsContract.hasRole(trustedRole, accounts[1].address)).to.equal(true)
+      expect(await myEAAccountsContract.hasRole(trustedRole, accounts[2].address)).to.equal(true)
+      expect(await myEAAccountsContract.hasRole(trustedRole, accounts[3].address)).to.equal(true)
+    })
+  
+    it("Accounts dont have wrong role", async function(){
+      expect(await myEAAccountsContract.hasRole(trustedRole, accounts[0].address)).to.equal(false)
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[1].address)).to.equal(false)
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[2].address)).to.equal(false)
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[3].address)).to.equal(false)
+    })  
+  })
+
+  describe("Grant role", function(){
+    it("Correct grant role by admin", async function(){
+      await myEAAccountsContract.grantRole(trustedRole, accounts[5].address)
+      expect(await myEAAccountsContract.hasRole(trustedRole, accounts[5].address)).to.equal(true)
+    })
+  
+    it("Correct dont grant role by trusted", async function(){
+      await expect(myEAAccountsContract.connect(accounts[2]).grantRole(trustedRole, accounts[5].address)).to.be.reverted
+    })
+  
+    it("Correct dont grant role by alies", async function(){
+      await expect(myEAAccountsContract.connect(accounts[7]).grantRole(trustedRole, accounts[5].address)).to.be.reverted
+    })  
+  })
+
+  describe("Revoke role", function(){
+    it("Correct revoke trusted account role by admin", async function(){
+      await myEAAccountsContract.revokeRole(trustedRole, accounts[1].address)
+      expect(await myEAAccountsContract.hasRole(trustedRole, accounts[1].address)).to.equal(false)
+    })
+
+    it("Correct dont revoke trusted account role by this trusted", async function(){
+      await expect(myEAAccountsContract.connect(accounts[1]).revokeRole(trustedRole, accounts[1].address)).to.be.reverted
+    })
+
+    it("Correct dont revoke trusted account role by other trusted", async function(){
+      await expect(myEAAccountsContract.connect(accounts[2]).revokeRole(trustedRole, accounts[1].address)).to.be.reverted
+    })
+  })
+
+
+  describe("Renounce role", function(){
+    it("Correct renounce role by right person", async function(){
+      await myEAAccountsContract.connect(accounts[1]).renounceRole(trustedRole, accounts[1].address)
+      expect(await myEAAccountsContract.hasRole(trustedRole, accounts[1].address)).to.equal(false)
+    })
+  
+    it("Correct dont renounce role by admin", async function(){
+      await expect(myEAAccountsContract.renounceRole(trustedRole, accounts[2].address)).to.be.reverted
+    })
+  
+    it("Correct dont renounce role by other person", async function(){
+      await expect(myEAAccountsContract.connect(accounts[6]).renounceRole(trustedRole, accounts[2].address)).to.be.reverted
+    })
+  })
+
+
+  describe("Recover voting", function(){
+    it("Correct full recovery voting", async function(){
+      await myEAAccountsContract.connect(accounts[1]).startRecoveryAccount(accounts[10].address)
+  
+      await myEAAccountsContract.connect(accounts[1]).voteRecoveryAccount(accounts[0].address, accounts[10].address)
+      await myEAAccountsContract.connect(accounts[2]).voteRecoveryAccount(accounts[0].address, accounts[10].address)
+      await myEAAccountsContract.connect(accounts[3]).voteRecoveryAccount(accounts[0].address, accounts[10].address)
+  
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[0].address)).to.equal(false)
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[10].address)).to.equal(true)
+    })
+  
+    it("Correct not full recovery voting", async function(){
+      await myEAAccountsContract.connect(accounts[1]).startRecoveryAccount(accounts[10].address)
+  
+      await myEAAccountsContract.connect(accounts[1]).voteRecoveryAccount(accounts[0].address, accounts[10].address)
+      await myEAAccountsContract.connect(accounts[2]).voteRecoveryAccount(accounts[0].address, accounts[10].address)
+  
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[0].address)).to.equal(true)
+      expect(await myEAAccountsContract.hasRole(adminRole, accounts[10].address)).to.equal(false)
+    })
+  })
+});
